e8ef49924856f1505ad9157c0dc18da1b95a3b7c
Patrick Vianna <patrickviannapblv@gmail.com>
2020-01-10 00:32:19 -0300

    Start project

---
+++ api/src/CopaFilmes.Domain/Entities/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CopaFilmes.Domain.Entities
+{
+    public class Movie
+    {
+        public string id { get; set; }
+        public string titulo { get; set; }
+        public int ano { get; set; }
+        public decimal nota { get; set; }
+
+        public Movie() { }
+    }
+}
